e1fe3600aeb52ab9ed7500288110825ec0c0b931
thushiJ <thushijayapathma50@gmail.com>
2025-05-12 22:44:23 +0530

    add feature hotel cards

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { assets } from "../assets/assets";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
@@ -19,21 +19,29 @@ const Navbar = () => {
 
     
 
-    const [isScrolled, setIsScrolled] = React.useState(false);
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const {openSignIn} = useClerk()
     const {user} = useUser()
     const navigate = useNavigate()
     const location = useLocation()
 
-    React.useEffect(() => {
+    useEffect(() => {
+
+        if(location.pathname !== '/'){
+            setIsScrolled(true);
+            return;
+        }else{
+            setIsScrolled(false)
+        }
+        setIsScrolled(prev => location.pathname !== '/' ? true:prev);
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 10);
         };
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, [location.pathname]);
 
     return (
                  
@@ -75,7 +83,14 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu Button */}
+                 
+
                 <div className="flex items-center gap-3 md:hidden">
+                    {user && <UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label= "My Bookings" labelIcon = {<BookIcon/>} onClick={()=>navigate('/my-bookings')}/>
+                        </UserButton.MenuItems>
+                    </UserButton>}
                 <img onClick= {()=> setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className= {`${isScrolled && 'invert'} h-4`}/>
                 </div>
 
@@ -91,13 +106,14 @@ const Navbar = () => {
                         </a>
                     ))}
 
-                    <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
+                    {user && <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all" 
+                    onClick={()=> navigate('/owner')}>
                         Dashboard
-                    </button>
+                    </button>}
 
-                    <button onClick = {openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    {!user && <button onClick = {openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
-                    </button>
+                    </button>}
                 </div>
             </nav>
         
